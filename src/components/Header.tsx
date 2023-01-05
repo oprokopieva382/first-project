9d9f29640a8b137cf9525830bd152fab039b73a6
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-01-05 11:55:31 -0600

    made css modules for all components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,9 @@
+import React from "react";
+import s from "./Header.module.css";
+
 const Header = () => {
   return (
-    <header className="header">
+    <header className={s.header}>
       <img
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgJELZzJvnJcRhgnkax8xXGfgdqHrcNf8iyA&usqp=CAU"
         alt=""
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,8 @@
+import React from "react";
+import s from "./Profile.module.css";
 const Profile = () => {
   return (
-    <div className="content">
+    <div className={s.content}>
       <div>
         <img
           src="https://static.scientificamerican.com/sciam/cache/file/5FE5FD06-AA07-43DE-9AF48B96EEDCF063_source.jpg"
@@ -10,10 +12,10 @@ const Profile = () => {
       <div>ava</div>
       <div>
         My posts
-        <div>New Post</div>
+        <div className={s.posts}>New Post</div>
         <div>
-          <div>Post 1</div>
-          <div>Post 2</div>
+          <div className={s.item}>Post 1</div>
+          <div className={s.item}>Post 2</div>
         </div>
       </div>
     </div>
