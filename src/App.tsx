ae413a71f462a11ffa3b24d9077aa724c7fa6678
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-01-05 09:58:31 -0600

    added style to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,46 @@
 import React from "react";
 import "./App.css";
-import Header from "./Header";
-import Technology from "./Technology";
 
 function App() {
-  console.log("App rendering");
   return (
-    <div>
-      <Header />
-      <Technology />
+    <div className="app-wrapper">
+      <header className="header">
+        <img src="" alt="" />
+      </header>
+      <nav className="nav">
+        <div>
+          <a href="">Profile</a>
+        </div>
+        <div>
+          <a href="">Messages</a>
+        </div>
+        <div>
+          <a href="">News</a>
+        </div>
+        <div>
+          <a href="">Music</a>
+        </div>
+        <div>
+          <a href="">Settings</a>
+        </div>
+      </nav>
+      <div className="content">
+        <div>
+          <img
+            src="https://static.scientificamerican.com/sciam/cache/file/5FE5FD06-AA07-43DE-9AF48B96EEDCF063_source.jpg"
+            alt=""
+          />
+        </div>
+        <div>ava</div>
+        <div>
+          My posts
+          <div>New Post</div>
+          <div>
+            <div>Post 1</div>
+            <div>Post 2</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
